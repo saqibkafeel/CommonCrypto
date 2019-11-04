6ea7ee8742831e8e898b989296a44f31b890d33d
Code Short <saqib.kafeel23@gmail.com>
2019-11-04 21:48:06 +0500

    Initial Commit

---
+++ encryptSwift5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  encryptSwift5
+//
+//  Created by saqib on 11/4/19.
+//  Copyright © 2019 saqib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
