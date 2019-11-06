a00ea2d3e2b8ae5f60bf78c44e031167c7c6b1c2
Code Short <saqib.kafeel23@gmail.com>
2019-11-06 18:58:57 +0500

    CommonCrypto

--- encryptSwift5/ViewController.swift
+++ encryptSwift5/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
-
+import CryptoSwift
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        do {
+            
+            let key1 = "XlQxN7a5181b07cj".utf8
+            let key2 = "XlQxN7a5181b07cj".utf8
+            let encrypted: Array<UInt8> = try! AES(key: Array("\(key1)\(key2)".utf8), blockMode: CBC(iv: Array("n5JJM04FN4kQ178d".utf8)), padding: .pkcs5).encrypt(Array("alexei1118@yandex.com|alexei123".utf8))
+            let hexString = encrypted.toHexString()
+            print("encrypted string : \(hexString)")
+           
+        } catch { }
     }
 
 
 }
-
